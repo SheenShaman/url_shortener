a80e19d2823b2b0866f9827d68676d5f1427b749
Danil Sheen <dan.80956@gmail.com>
2026-03-10 22:14:49 +0300

    add_tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,3 +14,30 @@ def test_shorten():
 
     assert result.status_code == 200
     assert short_url[:-6] == 'http://localhost:8000/'
+
+
+def test_redirect():
+    result = client.post(
+        "/shorten",
+        json={"url": "https://example.com"}
+    )
+    short_url = result.json()["short_url"]
+    code = short_url.split("/")[-1]
+    response = client.get(f"/{code}", follow_redirects=False)
+
+    assert response.status_code in [302, 307]
+
+
+def test_stats():
+    result = client.post(
+        "/shorten",
+        json={"url": "https://example.com"}
+    )
+    short_url = result.json()["short_url"]
+    code = short_url.split("/")[-1]
+    client.get(f"/{code}")
+    client.get(f"/{code}")
+    stats = client.get(f"/stats/{code}")
+
+    assert stats.status_code == 200
+    assert stats.json()["clicks"] == 2
